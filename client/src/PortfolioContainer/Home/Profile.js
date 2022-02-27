d4f8f6b91ae0aa46d91a74dc9d7aebd74ec416c1
Triston Golden <triston_golden@yahoo.com>
2022-02-26 22:08:37 -0500

    profile coming along

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Typical from "react-typical";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.linkedin.com/in/triston-golden-95111ab7/">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+            <a href="https://github.com/TGolden95">
+              <i className="fa fa-github-square"></i>
+            </a>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Welcome, My name is{" "}
+              <span className="highlighted-text">Triston</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Fullstack Web Developer 💻",
+                    1000,
+                    "HTML, CSS, Javascript 🖼",
+                    1000,
+                    "GA TECH Coding Bootcamp Graduate 🎓",
+                    1000,
+                    "MERN Stack Developer 📚",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Passion for connecting front and back end operations. 🤝
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Employable!{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
