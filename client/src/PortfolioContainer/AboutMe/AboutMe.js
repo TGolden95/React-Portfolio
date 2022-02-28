e3dda5a93e3373872584d704b22c382c260bd0d9
Triston Golden <triston_golden@yahoo.com>
2022-02-28 10:26:59 -0500

    coming along

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -11,10 +11,61 @@ export default function AboutMe(props) {
   };
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTANTS = {
+    description:
+      "Full stack web developer knowledgable in MERN stack, HTML, CSS, and Javascript.",
+    highlights: {
+      bullets: [
+        "Full Stack web development",
+        "Full Stack mobile development",
+        "Database Management",
+        "Building REST API's",
+        "React JS",
+      ],
+      heading: "Here are some of my skills:",
+    },
+  };
+
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
   return (
     <div className="about-me-container screen-container" id={props.id || ""}>
       <div className="about-me-parent">
         <ScreenHeading title={"AboutMe"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe}
+              >
+                {""}
+                Hire Me{" "}
+              </button>
+              <a href="Golden-Resume.pdf" download="Golden Golden-Resume.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
